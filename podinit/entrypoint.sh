27dfb19ce505ae952d404c9767972768d1a98557
Dimitris Petrou <petroudimitris@outlook.com>
2024-11-01 14:34:04 +0200

    Fixed minor bug in podinit entrypoint.sh that caused failure due to wrong argument passing

--- podinit/entrypoint.sh
+++ podinit/entrypoint.sh
@@ -2,10 +2,10 @@
 
 # Check the first argument
 if [ "$1" = "postgresql" ]; then
+  shift
   # Construct the DB_URL if the service type is postgresql
   DB_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DB}?sslmode=disable"
-
-  exec wait4x "$DB_URL" "$@"
+  exec wait4x postgresql "$DB_URL" "$@"
 else
   # For other types like "http" or "redis", just pass the arguments directly
   exec wait4x "$@"
